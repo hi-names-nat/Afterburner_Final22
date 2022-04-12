58757e3eab9b50c09a49bccc255c55e03d244abf
Natalie <natalie.soltis@outlook.com>
2022-04-12 16:23:33 -0400

    Initial Commit.

---
+++ src/GLViewNat22_Final.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "GLView.h"
+
+namespace Aftr
+{
+   class Camera;
+
+/**
+   \class GLViewNat22_Final
+   \author Scott Nykl 
+   \brief A child of an abstract GLView. This class is the top-most manager of the module.
+
+   Read \see GLView for important constructor and init information.
+
+   \see GLView
+
+    \{
+*/
+
+class GLViewNat22_Final : public GLView
+{
+public:
+   static GLViewNat22_Final* New( const std::vector< std::string >& outArgs );
+   virtual ~GLViewNat22_Final();
+   virtual void updateWorld(); ///< Called once per frame
+   virtual void loadMap(); ///< Called once at startup to build this module's scene
+   virtual void createNat22_FinalWayPoints();
+   virtual void onResizeWindow( GLsizei width, GLsizei height );
+   virtual void onMouseDown( const SDL_MouseButtonEvent& e );
+   virtual void onMouseUp( const SDL_MouseButtonEvent& e );
+   virtual void onMouseMove( const SDL_MouseMotionEvent& e );
+   virtual void onKeyDown( const SDL_KeyboardEvent& key );
+   virtual void onKeyUp( const SDL_KeyboardEvent& key );
+
+protected:
+   GLViewNat22_Final( const std::vector< std::string >& args );
+   virtual void onCreate();   
+};
+
+/** \} */
+
+} //namespace Aftr
